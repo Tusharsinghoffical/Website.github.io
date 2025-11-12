14365e597f09289814bcc95124ce4506aede410a
Tushar Singh <tusharsingh0101@gmail.com>
2025-11-12 14:19:01 +0530

    Fix SEO issues: Add robots.txt, sitemap.xml, and update canonical URLs for codewithmrsingh.tech

--- portfolio_freelance/urls.py
+++ portfolio_freelance/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -28,6 +29,8 @@ urlpatterns = [
     path("about/", include("about.urls")),
     path("contact/", include("contact.urls")),
     path("freelancing/", include("freelancing.urls")),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path("sitemap.xml", TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml")),
 ]
 
 # Serve static files during development and also when DEBUG is False for local testing
